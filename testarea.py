98359baa0228c75e7f5fdf830d3a50401f725f6f
youngil <bubbleutopia@naver.com>
2024-01-12 17:19:34 +0900

    evening work

--- testarea.py
+++ testarea.py
@@ -1,12 +1,16 @@
-result = True
-
-
-print(int(result==True))
-
-# for x in range(len(a)):
-#     if ValueError :
-#         pass
-#     else :
-#         a.remove(3+2)
+def solution(array):
+    repeat_list=[]
+    for x in range(1,100):
+        repeat_list.append(array.count(array(x))
+    if repeat_list.count(max(repeat_list))>1:
+        result = -1
+    else :
+        result = array.index(max(repeat_list))
     
-# del a.(b for b in a if b == score[0])
+    return result
+
+if __name__ == "__main__":
+    array = list(map(int,input().split()))
+    
+    answer = solution(array)
+    print (answer)
