5f575a80b6909b9ee305f44f292ef70719877324
YOUNGIL <bubbleutopia@naver.com>
2024-01-15 17:53:47 +0900

    plus

--- classmemo/class.py
+++ classmemo/class.py
@@ -1,4 +1,11 @@
-a=531
-b=set(str(a))
-b.update(list(str(a)))
-print(b)
+T = int(input())
+count = 1
+for t in range(T):
+    start = int(input())
+    source=set(str(start))
+        while True:
+            source.update(list(str(start)))
+            count += 1
+            start *= 
+            if len(source) == 10:
+                print(f'#{t+1} {count}')
