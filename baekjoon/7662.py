94ccc171188cde74afe9a195655a9c89f25cefcf
01 <bubbleutopia@naver.com>
2024-02-13 00:35:44 +0900

    update

--- baekjoon/7662.py
+++ baekjoon/7662.py
@@ -4,20 +4,27 @@ import heapq
 
 T = int(input())
 for t in range(T):
-    Q = []
+    Q_min = []
+    Q_max = []
     k = int(input())
-    for _ in range(k):
+    check = [1]*k
+    for i in range(k):
         command = input().split()
         if command[0]=='I':
-            heapq.heappush(Q,int(command[1]))
-        elif command[0]=='D':
-            if Q:
-                if command[1] == '1':
-                    max_val = max(Q)
-                    Q.remove(max_val)
-                else:
-                    heapq.heappop(Q)
-    if len(Q)==0:
+            heapq.heappush(Q_min,(int(command[1]),i))
+            heapq.heappush(Q_max,(-int(command[1]),i))
+        else:
+            if command[1] == '-1':
+                if Q_min:
+                    check[heapq.heappop(Q_min)[1]] = 0
+            elif command[1] == '1':
+                if Q_min:
+                    check[heapq.heappop(Q_max)[1]] = 0
+        while Q_min and check[Q_min[0][1]]==0:
+            heapq.heappop(Q_min)
+        while Q_max and check[Q_max[0][1]]==0:
+            heapq.heappop(Q_max)
+    if len(Q_min)==0:
         print('EMPTY')
     else:
-        print(max(Q), heapq.heappop(Q))
+        print(-Q_max[0][0], Q_min[0][0])
