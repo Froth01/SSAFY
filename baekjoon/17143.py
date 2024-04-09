cbc29c8ebd14cdc45d341a3b6a79e0c8a91456d6
01 <bubbleutopia@naver.com>
2024-04-09 17:41:32 +0900

    update 0409 1741

--- baekjoon/17143.py
+++ baekjoon/17143.py
@@ -1,32 +1,55 @@
+import sys
+input = sys.stdin.readline
+
 def fishman(x):
     global catch
     for i in range(R):
-        if frame[x][i]:
-            catch += frame[x][i][0]
-            sharks.remove([x,i])
-            frame[x][i] = []
+        if frame[i][x][2]>0:
+            catch += frame[i][x][2]
+            sharks.remove([i,x])
+            frame[i][x] = [0,0,0]
             return
 
 def sharkmove():
+    global sharks
+    new_shark = []
     for site in sharks:
-        shark = frame[site[0]][site[1]]
-        i,j = site[0]+delta[d][0], site[1]+delta[d][1]
+        i,j = site[0],site[1]
+        speed,dir,size = frame[i][j]
+        frame[i][j] = [0,0,0]
+        for s in range(speed):
+            i,j = i+delta[dir][0], j+delta[dir][1]
+            if i<0 or i>R-1 or j<0 or j>C-1:
+                if dir%2:
+                    dir+=1
+                else:
+                    dir-=1
+                i,j = i+delta[dir][0]*2, j+delta[dir][1]*2
+        if frame[i][j][2]>size:
+            continue
+        elif frame[i][j] != [0,0,0]:
+            if [i,j] in new_shark:
+                new_shark.remove([i,j])
+        frame[i][j] = [speed,dir,size]
+        new_shark.append([i,j])
+    sharks = new_shark
 
 
 
 
 R,C,M = map(int,input().split())
-frame = [[[] for _ in range(C)] for _ in range(R)]
+if M == 0:
+    print(0)
+    exit()
+frame = [[[0,0,0] for _ in range(C)] for _ in range(R)]
 delta = [(0,0),(-1,0),(1,0),(0,1),(0,-1)]
 sharks = []
 for m in range(M):
     r,c,s,d,z = map(int,input().split())
-    frame[r][c] = [s,d,z]
-    sharks.append([r,c])
-time = 1
+    frame[r-1][c-1] = [s,d,z]
+    sharks.append([r-1,c-1])
 catch = 0
-while time <= C:
-    fishman(time)
+for t in range(C):
+    fishman(t)
     sharkmove()
-    time += 1
 print(catch)
